b451eb5496c75ba3f1d294c620347ef6006a484c
Szymon Skaziński <63155273+Szymon-Skazinski@users.noreply.github.com>
2023-05-26 12:52:14 +0200

    Refactored Task3, added Measurements class

---
+++ src/main/java/skazinski/szymon/task1/Measurements.java
@@ -0,0 +1,50 @@
+package skazinski.szymon.task1;
+
+import java.util.List;
+import java.util.Objects;
+
+public class Measurements {
+    private final List<Integer> distinctIntegers;
+    private final int count;
+    private final int distinct;
+    private final int min;
+    private final int max;
+
+    public Measurements(List<Integer> distinctAndSortedList, int count, int distinct, int min, int max) {
+        this.distinctIntegers = distinctAndSortedList;
+        this.count = count;
+        this.distinct = distinct;
+        this.min = min;
+        this.max = max;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Measurements that = (Measurements) o;
+        return count == that.count && distinct == that.distinct && min == that.min && max == that.max
+                && distinctIntegers.equals(that.distinctIntegers);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(distinctIntegers, count, distinct, min, max);
+    }
+
+    @Override
+    public String toString() {
+        return listToString(distinctIntegers) + "\n" +
+                "count: " + count + "\n" +
+                "distinct: " + distinct + "\n" +
+                "min: " + min + "\n" +
+                "max: " + max;
+    }
+
+    private String listToString(List<Integer> integers) {
+        return integers.toString()
+                .replace("[", "")
+                .replace("]", "")
+                .replace(",", "");
+    }
+}
--- src/main/java/skazinski/szymon/task1/Task1.java
+++ src/main/java/skazinski/szymon/task1/Task1.java
@@ -1,50 +1,23 @@
 package skazinski.szymon.task1;
 
-import skazinski.szymon.utils.LineReader;
-
-import java.io.InputStream;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Task1 {
 
-    public void getMeasurements(InputStream inputStream) {
-        LineReader lineReader = new LineReader(inputStream);
-        String[] strings = lineReader.readLine();
+    public Measurements getBasicMeasurements(List<Integer> integers) {
+        int count = integers.size();
 
-        int count = strings.length;
-
-        List<Integer> integers = Arrays.stream(strings)
-                .map(Integer::parseInt)
+        List<Integer> sortedAndDistinct = integers.stream()
                 .distinct()
                 .sorted()
                 .collect(Collectors.toList());
 
-        int distinctCount = integers.size();
-        Integer min = integers.get(0);
-        Integer max = integers.get(distinctCount - 1);
+        int distinctCount = sortedAndDistinct.size();
+        Integer min = sortedAndDistinct.get(0);
+        Integer max = sortedAndDistinct.get(distinctCount - 1);
 
-        String integersString = listToString(integers);
-
-        printOutput(count, distinctCount, min, max, integersString);
-    }
-
-    private String listToString(List<Integer> integers) {
-        return integers.toString()
-                .replace("[", "")
-                .replace("]", "")
-                .replace(",", "");
-    }
-
-    private void printOutput(int count, int distinctCount, Integer min, Integer max, String integersString) {
-        String output = integersString + "\n" +
-                "count: " + count + "\n" +
-                "distinct: " + distinctCount + "\n" +
-                "min: " + min + "\n" +
-                "max: " + max;
-
-        System.out.println(output);
+        return new Measurements(sortedAndDistinct,count,distinctCount,min,max);
     }
 
 }
