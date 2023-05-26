864a5ec054eac671a9db690b9332c606aeb955d0
Szymon Skaziński <63155273+Szymon-Skazinski@users.noreply.github.com>
2023-05-26 10:09:10 +0200

    Refactored Task3, created separate Graph class

--- src/main/java/skazinski/szymon/task3/Task3.java
+++ src/main/java/skazinski/szymon/task3/Task3.java
@@ -3,23 +3,33 @@ package skazinski.szymon.task3;
 import skazinski.szymon.utils.LineReader;
 
 import java.io.InputStream;
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Task3 {
 
-    public void getNumberOfGraphs(InputStream inputStream) {
+    public int getNumberOfGraphs(InputStream inputStream) {
         LineReader lineReader = new LineReader(inputStream);
-
         int linesCount = lineReader.readNextInt();
-
         List<Graph> graphs = getGraphsFromInput(lineReader, linesCount);
 
         graphs.sort(Comparator.comparingInt(Graph::getFirst));
 
         List<Graph> joinedGraphs = joinGraphs(graphs);
 
-        System.out.println(joinedGraphs.size());
+        return joinedGraphs.size();
+    }
+
+    private List<Graph> getGraphsFromInput(LineReader lineReader, int linesCount) {
+        List<Graph> graphs = new LinkedList<>();
+        for (int i = 0; i < linesCount; i++) {
+            Graph graph = new Graph();
+            graph.addVertex(lineReader.readNextInt());
+            graph.addVertex(lineReader.readNextInt());
+            graphs.add(graph);
+        }
+        return graphs;
     }
 
     private List<Graph> joinGraphs(List<Graph> graphs) {
@@ -40,47 +50,5 @@ public class Task3 {
             joinedGraphs.add(graph);
         }
         return joinedGraphs;
-    }
-
-    private List<Graph> getGraphsFromInput(LineReader lineReader, int linesCount) {
-        List<Graph> graphs = new LinkedList<>();
-        for (int i = 0; i < linesCount; i++) {
-            Graph graph = new Graph();
-            graph.addVertex(lineReader.readNextInt());
-            graph.addVertex(lineReader.readNextInt());
-            graphs.add(graph);
-        }
-        return graphs;
-    }
-}
-
-class Graph implements Comparable<Graph> {
-
-    SortedSet<Integer> vertices = new TreeSet<>();
-
-    public void addVertex(Integer value) {
-        vertices.add(value);
-    }
-
-    public void joinGraphs(Graph graph) {
-        vertices.addAll(graph.vertices);
-    }
-
-    public Integer getLast() {
-        return vertices.last();
-    }
-
-    public Integer getFirst() {
-        return vertices.first();
-    }
-
-    @Override
-    public String toString() {
-        return vertices.stream().map(Object::toString).collect(Collectors.joining("-", "[", "]"));
-    }
-
-    @Override
-    public int compareTo(Graph o) {
-        return this.getFirst() - o.getFirst();
     }
 }
